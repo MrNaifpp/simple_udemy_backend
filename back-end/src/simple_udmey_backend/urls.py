f6796e232bc0c6140bd8465149e377bfeac79d56
Naif1997 <naifali18@gmail.com>
2021-02-12 18:36:25 +0300

    add route to main urls file

--- back-end/src/simple_udmey_backend/urls.py
+++ back-end/src/simple_udmey_backend/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/',include('course.api.urls'))
+    path('api/',include('course.api.urls')),
+    path('',include('accounts.urls'))
 ]
