eb91b4caa336fc752f1bef4a50982c84fdb0c47c
MrNaifpp <47330345+MrNaifpp@users.noreply.github.com>
2021-02-20 18:38:50 +0300

    Migrate to mysql (#1)
    * hide secret informatiom in envirment file
    
    * add new endpoint to show all users we have

--- back-end/src/accounts/urls.py
+++ back-end/src/accounts/urls.py
@@ -6,6 +6,7 @@ from .views import LoginAPI
 
 urlpatterns = [
     path('api/register/', RegisterAPI.as_view(), name='register'),
+    path('api/allUser/', RegisterAPI.AllUsersAPI.as_view(), name='allUser'),
     path('api/login/', LoginAPI.as_view(), name='login'),
     path('api/logout/', knox_views.LogoutView.as_view(), name='logout'),
     path('api/logoutall/', knox_views.LogoutAllView.as_view(), name='logoutall'),
